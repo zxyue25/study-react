e916fb21a186bce300a3f1a823620887cd7a6b16
zxyue25 <zxy@syzc.net.cn>
2022-01-16 16:55:56 +0800

    feat: 基础组件编写

--- src/index.js
+++ src/index.js
@@ -1,19 +1,5 @@
-// 入口文件
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 
-ReactDOM.render(
-  // 严格模式，开启帮助检查一些React即将废弃的API等，比如ref不能用字符串类型等
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App />, document.getElementById('root'))
