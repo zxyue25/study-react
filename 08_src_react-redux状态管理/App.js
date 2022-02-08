b3b512fa231dbf38a985fd040bbf23f6984b2f2d
zxyue25 <zxy@syzc.net.cn>
2022-02-09 00:44:44 +0800

    docs

---
+++ 08_src_react-redux状态管理/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Count from "./container/count";
+import Person from './container/person'
+
+export default class App extends Component {
+  render() {
+    return <div>
+      <Count />
+      <Person />
+    </div>;
+  }
+}
---
+++ 08_src_react-redux状态管理/redux/constants.js
@@ -0,0 +1,5 @@
+// 定义action对象中type类型的常量值，
+export const INCREMENT = 'increment'
+export const DECREMENT = 'decrement'
+
+export const ADD_PERSON = 'add_person'
