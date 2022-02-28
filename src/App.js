e494e972e7585f451fa81f4138e4a2f839e89d15
zhengxiuyue <zhengxiuyue@jd.com>
2022-02-28 10:55:15 +0800

    feat: lint

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 
 export default class App extends Component {
-  constructor(props) {
+  constructor (props) {
     super(props)
     this.handleClick1 = this.handleClick1.bind(this)
   }
@@ -9,7 +9,7 @@ export default class App extends Component {
   handleSubmit (e) {
     console.log(e)
     // e.preventDefault();
-    console.log('You clicked submit.');
+    console.log('You clicked submit.')
   }
 
   handleClick () {
@@ -37,21 +37,21 @@ export default class App extends Component {
 
   render () {
     return <div>
-      <form onSubmit={this.handleSubmit}>
+      <form onSubmit={ this.handleSubmit }>
         <button type="submit">显示阻止默认行为</button>
       </form>
       
       <h1>this指向undefined</h1>
-      <button onClick={this.handleClick}>this指向</button><br />
+      <button onClick={ this.handleClick }>this指向</button><br />
       <h2>不传参</h2>
-      <button onClick={this.handleClick1}>this指向解方1</button>bind不方便，写法不优雅<br /> 
-      <button onClick={this.handleClick2}>this指向解法2</button>不传参优先这么写<br /> 
-      <button onClick={() => this.handleClick()}>this指向解法3</button><br />
+      <button onClick={ this.handleClick1 }>this指向解方1</button>bind不方便，写法不优雅<br /> 
+      <button onClick={ this.handleClick2 }>this指向解法2</button>不传参优先这么写<br /> 
+      <button onClick={ () => this.handleClick() }>this指向解法3</button><br />
 
       <h2>传参</h2>
-      <button onClick={(e) => this.handleClick2(e, '111')}>this指向解法4</button>传参优先这么写<br /> 
-      <button onClick={this.handleClick3('1111')}>this指向解法5</button>传参<br /> 
+      <button onClick={ (e) => this.handleClick2(e, '111') }>this指向解法4</button>传参优先这么写<br /> 
+      <button onClick={ this.handleClick3('1111') }>this指向解法5</button>传参<br /> 
 
-    </div>;
+    </div>
   }
 }
